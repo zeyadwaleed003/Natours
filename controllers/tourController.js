db3aa8c08f934d77b7f84c6379afa085d4fc28ab
Zeyad Waleed <zeyadw526@gmail.com>
2025-03-04 00:21:17 +0200

    feat(api): allow updating images for tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,62 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) cb(null, true);
+  else cb(new AppError('Not an image! Please upload only images', 400), false);
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+]);
+
+exports.resizeTourImages = async (req, res, next) => {
+  console.log(req.files.images);
+  if (!req.files.imageCover || !req.files.images) return next();
+
+  // 1) Cover Image
+  const imageCoverName = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${imageCoverName}`);
+
+  // Saving to database
+  req.body.imageCover = imageCoverName;
+
+  // 2) Images
+  // Declaring an images field in req.body in order to push images to it and save to the database
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const imageName = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${imageName}`);
+
+      // Saving to database
+      req.body.images.push(imageName);
+    }),
+  );
+
+  next();
+};
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.sort = '-ratingsAverage,price';
